79830cb056fe15bb7b24b495b596752e39a51143
shreybertocarlos <shreydip1537@gmail.com>
2025-04-27 10:22:09 +0545

    infoform updates

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'slug',
+        'logo',
+        'banner',
+        'bio',
+        'vision',
+
+    ];
 }
