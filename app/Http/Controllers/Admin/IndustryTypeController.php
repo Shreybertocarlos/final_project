230861e9101aeefb1e2a725ea9dc11ce6ac5394b
shreybertocarlos <shreydip1537@gmail.com>
2025-04-28 18:30:31 +0545

    delete feature pt1

--- app/Http/Controllers/Admin/IndustryTypeController.php
+++ app/Http/Controllers/Admin/IndustryTypeController.php
@@ -53,17 +53,15 @@ class IndustryTypeController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
-        //
-    }
+
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id) : View
     {
-        //
+        $industryType = IndustryType::findOrFail($id);
+        return view('admin.industry-type.edit', compact('industryType'));
     }
 
     /**
@@ -71,7 +69,18 @@ class IndustryTypeController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            //add id when checking for uniquesness
+            'name' => ['required', 'max:255', 'unique:industry_types,name,'.$id]
+        ]);
+
+        $type = IndustryType::findOrFail($id);
+        $type->name = $request->name;
+        $type->save();
+
+        Notify::updatedNotification();
+
+        return to_route('admin.industry-types.index');
     }
 
     /**
