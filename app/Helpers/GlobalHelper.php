8dc26f1bffdac538b1164d57b6229129af31d29f
shreybertocarlos <shreydip1537@gmail.com>
2025-04-28 17:47:21 +0545

    helperfile

---
+++ app/Helpers/GlobalHelper.php
@@ -0,0 +1,10 @@
+<?php
+
+/** Check input error */
+
+if(!function_exists('hasError')) {
+    function hasError($errors, string $name) : ?String
+    {
+        return $errors->has($name) ? 'is-invalid' : '';
+    }
+}
