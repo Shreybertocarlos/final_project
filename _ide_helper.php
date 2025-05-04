565c753eda13e132d1ee5d85ea93a5e374f0d10d
shreybertocarlos <shreydip1537@gmail.com>
2025-05-04 20:57:22 +0545

    paypal some work done ig

--- _ide_helper.php
+++ _ide_helper.php
@@ -17725,6 +17725,34 @@ namespace Spatie\LaravelIgnition\Facades {
             }
     }
 
+namespace Srmklive\PayPal\Facades {
+            /**
+     * 
+     *
+     */        class PayPal {
+                    /**
+         * Get specific PayPal API provider object to use.
+         *
+         * @throws Exception
+         * @return \Srmklive\PayPal\Services\PayPal 
+         * @static 
+         */        public static function getProvider()
+        {
+                        return \Srmklive\PayPal\PayPalFacadeAccessor::getProvider();
+        }
+                    /**
+         * Set PayPal API Client to use.
+         *
+         * @throws \Exception
+         * @return \Srmklive\PayPal\Services\PayPal 
+         * @static 
+         */        public static function setProvider()
+        {
+                        return \Srmklive\PayPal\PayPalFacadeAccessor::setProvider();
+        }
+            }
+    }
+
 namespace Illuminate\Http {
             /**
      * 
@@ -21286,6 +21314,7 @@ namespace  {
             class Flasher extends \Flasher\Laravel\Facade\Flasher {}
             class Notyf extends \Flasher\Notyf\Laravel\Facade\Notyf {}
             class Flare extends \Spatie\LaravelIgnition\Facades\Flare {}
+            class PayPal extends \Srmklive\PayPal\Facades\PayPal {}
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\PaymentController;
 use App\Http\Controllers\Fronted\CandidateDashboardController;
 use App\Http\Controllers\Frontend\CandidateEductionController;
 use App\Http\Controllers\Frontend\CandidateExperienceController;
@@ -90,5 +91,20 @@ Route::group(
     Route::post('/profile/founding-info', [CompanyProfileController::class, 'updateFoundingInfo'])->name('profile.founding-info');
     Route::post('/profile/account-info', [CompanyProfileController::class, 'updateAccountInfo'])->name('profile.account-info');
     Route::post('/profile/password-update', [CompanyProfileController::class, 'updatePassword'])->name('profile.password-update');
+
+     /**Payment Routes */
+     Route::get('payment/success', [PaymentController::class, 'paymentSuccess'])->name('payment.success');
+     Route::get('payment/error', [PaymentController::class, 'paymentError'])->name('payment.error');
+
+     Route::get('paypal/payment', [PaymentController::class, 'payWithPaypal'])->name('paypal.payment');
+     Route::get('paypal/success', [PaymentController::class, 'paypalSuccess'])->name('paypal.success');
+     Route::get('paypal/cancel', [PaymentController::class, 'paypalCancel'])->name('paypal.cancel');
+
+     Route::get('stripe/payment', [PaymentController::class, 'payWithStripe'])->name('stripe.payment');
+     Route::get('stripe/success', [PaymentController::class, 'stripeSuccess'])->name('stripe.success');
+     Route::get('stripe/cancel', [PaymentController::class, 'stripeCancel'])->name('stripe.cancel');
+
+     Route::get('razorpay-redirect', [PaymentController::class, 'razorpayRedirect'])->name('razorpay-redirect');
+     Route::post('razorpay/payment', [PaymentController::class, 'payWithRazorpay'])->name('razorpay.payment');
     }
 );
